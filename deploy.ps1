42f20ce40b8bef6985ef0f966a280b414da7a64e
luiz.monad <luizfelipestang@gmail.com>
2019-03-13 14:20:54 -0300

    added files

---
+++ deploy.ps1
@@ -0,0 +1,10 @@
+﻿
+$external = "$PSScriptRoot\app\.externalNativeBuild"
+$target = "$PSScriptRoot\output"
+
+md $target -force
+
+('arm64-v8a', 'armeabi-v7a') | % { 
+    cp "$external\cmake\release\$_\opencv\lib\$_\*"          "$target\$_" -Verbose -Force
+    cp "$external\cmake\release\$_\opencv\3rdparty\lib\$_\*" "$target\$_" -Verbose -Force
+}
