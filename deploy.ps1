f70220d58ac5aeef67e49edcc2e3c470fe7b289f
luiz <tokadesenvolvedor@gmail.com>
2019-03-13 16:54:13 -0300

    fix build for the built server

--- deploy.ps1
+++ deploy.ps1
@@ -2,9 +2,8 @@
 $external = "$PSScriptRoot\app\.externalNativeBuild"
 $target = "$PSScriptRoot\output"
 
-md $target -force
-
 ('arm64-v8a', 'armeabi-v7a') | % { 
+    md "$target\$_" -force
     cp "$external\cmake\release\$_\opencv\lib\$_\*"          "$target\$_" -Verbose -Force
     cp "$external\cmake\release\$_\opencv\3rdparty\lib\$_\*" "$target\$_" -Verbose -Force
 }
